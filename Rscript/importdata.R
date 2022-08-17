352b3074cf5a4284bcceaec4f2c593620a0636cd
jalapic <curleyjp0@gmail.com>
2022-08-17 12:46:57 -0500

    first commit

---
+++ Rscript/importdata.R
@@ -0,0 +1,6 @@
+# import data
+
+cats <- read.csv("https://raw.githubusercontent.com/jalapic/learnR/master/datasets/aac_shelter_cat_outcome_eng.csv")
+
+## Write this csv file to "raw_data"
+write.csv(cats, "raw_data/cats.csv", row.names = F)
